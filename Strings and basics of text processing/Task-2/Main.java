5be752e2d21ce8ba54355b5f362c579c69529e15
Mikhail Kutovenko <70900496+mkhlktvnk@users.noreply.github.com>
2022-07-02 22:18:04 +0300

    Create Main.java

---
+++ Strings and basics of text processing/Task-2/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        String text = "word abc letter word word word abcs qweasd /// klf;s";
+        System.out.println(text.replace("word", "letter"));
+    }
+}
